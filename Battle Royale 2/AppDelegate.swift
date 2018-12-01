bd94736e51c73525dbf4d09ff51707729b1d6ae0
Morsmordre <kyle.is.gfy@gmail.com>
2018-11-30 21:15:07 -0800

    Initial Commit

---
+++ Battle Royale 2/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Battle Royale 2
+//
+//  Created by Kyle Schneider on 11/30/18.
+//  Copyright © 2018 Kyle Schneider›‹. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Battle Royale 2Tests/Battle_Royale_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Battle_Royale_2Tests.swift
+//  Battle Royale 2Tests
+//
+//  Created by Kyle Schneider on 11/30/18.
+//  Copyright © 2018 Kyle Schneider›‹. All rights reserved.
+//
+
+import XCTest
+@testable import Battle_Royale_2
+
+class Battle_Royale_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
